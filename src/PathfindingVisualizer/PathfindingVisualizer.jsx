8496d3db4140076edb49e0cf7807a0be336f8e82
David D'Souza <1david.dsouza.2000@gmail.com>
2021-06-30 16:08:31 -0400

    start/finish nodes will node show after running algorithm

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -76,8 +76,7 @@ export default function PathfindingVisualizer() {
             }
             setTimeout(() => {
                 const node = visitedNodesInOrder[i];
-                document.getElementById(`node-${node.row}-${node.col}`).className =
-                    'node node-visited';
+                document.getElementById(`node-${node.row}-${node.col}`).classList.add('node-visited');
             }, 10 * i);
         }
     }
@@ -86,8 +85,7 @@ export default function PathfindingVisualizer() {
         for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
             setTimeout(() => {
                 const node = nodesInShortestPathOrder[i];
-                document.getElementById(`node-${node.row}-${node.col}`).className =
-                    'node node-shortest-path';
+                document.getElementById(`node-${node.row}-${node.col}`).classList.add('node-shortest-path');
             }, 50 * i);
         }
     }
